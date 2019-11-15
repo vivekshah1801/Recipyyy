d0b8632cfc61efb3df0f5a2075515adec6356d48
vivekshah1801 <vivekshah9969@gmail.com>
2019-11-15 16:10:53 +0530

    Add project files.

---
+++ Recipyyy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recipyyy.Startup))]
+namespace Recipyyy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
